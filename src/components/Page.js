df2acd67eb755bdb3498be6d5ba78ea071174014
Tushar Kale <tushar.kale@simplifyvms.com>
2025-01-09 20:21:36 +0530

    Demo web

---
+++ src/components/Page.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import './Page.css';
+
+const Page = () => {
+  return (
+    <>
+      <nav className="navigation container">
+        <div className="nav-brand">
+          <a href="/">adarshbalika</a>
+        </div>
+        <ul className="list-non-bullet nav-pills">
+          <li className="list-item-inline">
+            <a className="link link-active" href="/">home</a>
+          </li>
+          <li className="list-item-inline">
+            <a className="link" href="/projects">projects</a>
+          </li>
+          <li className="list-item-inline">
+            <a className="link" href="/blogs">blogs</a>
+          </li>
+        </ul>
+      </nav>
+
+      <header className="hero">
+        <img
+          src="https://adarshbalika.netlify.app/images/hero.svg"
+          height="600px"
+          width="600px"
+          
+          alt="React Logo"
+        />
+        <h1 className="hero-heading">
+          adarsh balika the <span className="heading-inverted">web developer</span>
+        </h1>
+      </header>
+
+      <section className="section ow">
+        <div className="container container-center">
+          <h1>technologies</h1>
+          <p>
+            I'm familiar with HTML5, CSS3, Git, JavaScript, NodeJS, ReactJS, and Web Hosting
+          </p>
+        </div>
+      </section>
+
+      <section className="section">
+        <div className="container container-center">
+          <h1>projects</h1>
+          <p>
+            I like to showcase my work and thus, you can see my projects hosted online
+          </p>
+          <a className="link link-primary" href="/projects">See Projects</a>
+        </div>
+      </section>
+
+      <section className="section ow">
+        <div className="container container-center">
+          <h1>blogs</h1>
+          <p>
+            I am also working on some technical and non-technical blogs. I like to document my journey of learning.
+          </p>
+          <a className="link link-secondary" href="/blogs">Read Blogs</a>
+        </div>
+      </section>
+
+      <footer className="footer">
+        <div className="footer-header">social media presence</div>
+        <ul className="social-links list-non-bullet">
+          <li className="list-item-inline"></li>
+          <li className="list-item-inline"></li>
+          <li className="list-item-inline">
+            <a className="link" href="https://www.linkedin.com/in/rutika-varpe-05a154331/">
+              linkedin
+            </a>
+            <a className="link" href="https://github.com/Rutika-Varpe">Github</a>
+          </li>
+        </ul>
+      </footer>
+    </>
+  );
+};
+
+export default Page;
